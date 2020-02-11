4ea115c1ff534f0cdcdaacd4d92559169e5efd3a
Abdelkhalek BENHOUMINE <abbenhoumi@a106mac07.local>
2020-02-11 17:07:56 +0100

    detail user

--- app/src/main/java/com/abdelkhalek_bane/tp_android/User.java
+++ app/src/main/java/com/abdelkhalek_bane/tp_android/User.java
@@ -5,10 +5,18 @@ class User {
     public String first_name ;
     public String last_name  ;
     public String avatar  ;
+    public String email  ;
 
     public User(String name , String last_name , String avatar){
         this.first_name = name  ;
         this.last_name  = last_name ;
         this.avatar = avatar ;
+    }
+
+    public User(String name , String last_name , String avatar ,String email ){
+        this.first_name = name  ;
+        this.last_name  = last_name ;
+        this.avatar = avatar ;
+        this.email  = email  ;
     }
 }
