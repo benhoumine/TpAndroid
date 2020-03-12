12f2c2166d8af5e56928032b3f8e842ea1d48000
Bane <mamadoubane61@yahoo.fr>
2020-03-12 19:01:35 +0100

    detail activity done

--- app/src/main/java/com/abdelkhalek_bane/tp_android/DetailActivity.java
+++ app/src/main/java/com/abdelkhalek_bane/tp_android/DetailActivity.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
     private User user;
+    private TextView textViewPrenom;
+    private TextView textViewNom;
+    private TextView textViewEmail;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,19 @@ public class DetailActivity extends AppCompatActivity {
         String userJSON = getIntent().getExtras().getString("USER_JSON");
         Gson gson = new Gson();
         this.user = gson.fromJson(userJSON, User.class);
+
+        //Récupération des vues
+        View detailsView = (View)findViewById(R.id.details);
+        textViewPrenom = (TextView)detailsView.findViewById(R.id.textViewPrenom);
+        textViewNom = (TextView)detailsView.findViewById(R.id.textViewNom);
+        textViewEmail = (TextView)detailsView.findViewById(R.id.textViewEmail);
+        imageView = (ImageView)detailsView.findViewById(R.id.imageView);
+
+        //Mis à jour des vues
+        textViewPrenom.setText(textViewPrenom.getText()+this.user.first_name);
+        textViewNom.setText(textViewNom.getText()+this.user.last_name);
+        textViewEmail.setText(textViewEmail.getText()+this.user.email);
+        Picasso.get().load(this.user.avatar).into(imageView);
     }
 
 }
